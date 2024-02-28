58efc3819871645995088d0f684e08a3028fbda0
Jorge Gomez <jorge.electron@gmail.com>
2024-02-27 23:38:14 -0800

    adding data types

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,28 @@
+// # Values and Variables
+// Declare variables called country, 
+// continent and population and assign their values 
+// according to your own country (population in millions).
+// Log their values to the console.
+
+let country = "Mexico";
+let continent = "America";
+let poulation = 130;
+let $function = true;
+
+console.log('Country: ', country);
+console.log('Continent: ', continent);
+console.log('Population: ', poulation);
+console.log('$function :>> ', $function);
+
+// # Data Types
+// Declare a variable called isIsland and set its value according to your country. 
+// The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+// Log the types of isIsland, population, country and language to the console.
+
+let isIsland = false
+let language
+
+console.log('typeof country :>> ', typeof country); //boolean
+console.log('typeof isIsland :>> ', typeof isIsland); //boolean
+console.log('typeof language :>> ', typeof language); //undefined
+
